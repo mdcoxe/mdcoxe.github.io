75e3914aa49e011f26417cb45bf0a861cc1391ea
Michael Coxe <mcoxe@me.com>
2021-01-31 12:54:04 -0900

    added in experience section...just need to add in stome styling...looks plain

--- src/Components/Experience/index.js
+++ src/Components/Experience/index.js
@@ -1,30 +1,53 @@
 import './styles.css';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 const Experience = () => {
+    
 
     return (
         <div id="experience" className="xp-container">
-                Experience
-                <div className="home-container">
-                <div className='typed'> 
-                    {/* <h1>I'm a <Typed
-                                strings={words}
-                                typeSpeed={100}
-                                backSpeed={50}
-                                loop='true'
-                            /> */}
-                    {/* </h1>  */}
-                </div>
-                <p className='home-p'>
-                    Software Engineer, Full-Stack Developer, Veteran, Freelancer, Volunteer 
+          <div className="skew"></div>
+            <h1>Experience</h1>
+         
+            <div className="resume">
+                <p>
+                    Download and view my resume
                 </p>
-                <p className='home-brand'>
-                    I am a detailed-oriented, full-stack developer with proven ability to adapt and learn quickly. My military and private sector experience provided me with the skills to communicate effectively in diverse environments and solve problems efficiently.
+                <a href="/MichaelCoxeResume.pdf" download>
+                    <img alt='resume thumbnail' src="/resumeimg.png"/>
+                </a>
+            </div>
+            <hr/>
+            <div className="linkedin">
+                <a href="http://linkedin.com/in/michaelcoxe" target='blank'>
+                    <FontAwesomeIcon
+                        icon={faLinkedin}
+                        className='xp-icon linkedin-icon'
+                        size="6x"
+                        />
+                </a>
+                <p>
+                    Connect with me on LinkedIn
                 </p>
             </div>
+            <hr/>
+            <div className="github">
+                <p>
+                    Check-out my repositories on Github
+                </p>
+                <a href="http://github.com/mdcoxe" target='blank'>
+                    <FontAwesomeIcon
+                        icon={faGithub}
+                        className='xp-icon github-icon'
+                        size='6x'
+                        />
+                </a>
             </div>
-        
+       
+        </div>      
     ) 
-}
+};
 
 export default Experience;
--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -4,6 +4,7 @@ const Projects = () => {
 
     return  <div id="projects" className="projects-container">
                 Projects
+              
             </div>
 }
 
