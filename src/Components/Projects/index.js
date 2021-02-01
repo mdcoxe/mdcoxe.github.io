f13a2649fc57f43ddf4c782e4d4210a37fbeafb4
Michael Coxe <mcoxe@me.com>
2021-01-31 20:50:17 -0900

    built mock up of project cards

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,30 +1,128 @@
 import './styles.css';
 import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
+import { Row, Col, Container } from 'react-bootstrap';
 
 const Projects = () => {
-
     return  (
         <div id="projects" className="projects-container">
             <h1 className='project-heading'> 
                 Projects
             </h1>
-            <div className="project-cards">
-                <CardGroup>
-                    <Card className='p-card' style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+            <Container  className="project-cards">
+               <Row>
+                   <Col sm>
+                        <Card className='card-card'>
+                            <Card.Img className='card-img' variant="top" src="/assets/expkittensmall.png" />
+                            <Card.Body className='card-body'>
+                                <Card.Title className='card-title'>
+                                    Exploding Kittens
+                                </Card.Title>
+                                <hr/>
+                                <Card.Text className='card-text'>
+                                    Two player card game built to solidify experience using javascript, HTML and CSS.  
+                                </Card.Text>
+                                <hr/>
+                                <Card.Link className='link' href='https://mdcoxe.github.io/explodingkittens/'>
+                                    Deployed
+                                </Card.Link>
+                                <Card.Link className='link' href='https://github.com/mdcoxe/explodingkittens'>
+                                    GitHub Repo
+                                </Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm>    
+                        <Card className='card-card'>
+                            <Card.Img className='card-img' variant="top" src="/assets/honeybadgersmall.png" />
+                            <Card.Body className='card-body'>
+                                <Card.Title className='card-title'>
+                                    Honey Badger Fat-Tire Bike Shop
+                                </Card.Title>
+                                <hr/>
+                                <Card.Text className='card-text'>
+                                    Basic e-commerce store, using NodeJS, Express, MongoDB, and RESTful routing.
+                                </Card.Text>
+                                <hr/>
+                                <Card.Link className='link' href='https://github.com/mdcoxe/honey-badger-fat-tire-bikes'>
+                                    Deployed
+                                </Card.Link>
+                                <Card.Link className='link' href='https://honey-badger-bikes.herokuapp.com/fattire/'>
+                                    GitHub Repo
+                                </Card.Link>
+                            </Card.Body>
+                        </Card> 
+                    </Col>
+                    <Col sm>
+                        <Card className='card-card'>
+                            <Card.Img className='card-img' variant="top" src="/assets/astrowxsmall.png" />
+                            <Card.Body className='card-body'>
+                                <Card.Title className='card-title'>
+                                    Astro-Wx
+                                </Card.Title>
+                                <hr/>
+                                <Card.Text className='card-text'>
+                                    Full-Stack MERN app, integrating multiple API’s to increase chances ofviewing Aurora Borealis and other astronomical events. 
+                                </Card.Text>
+                                <hr/>
+                                <Card.Link className='link' href='https://github.com/mdcoxe/Astro-Wx'>
+                                    Deployed
+                                </Card.Link>
+                                <Card.Link className='link' href='https://astro-wx.herokuapp.com/'>
+                                    GitHub Repo
+                                </Card.Link>
+                            </Card.Body>
+                        </Card> 
+                    </Col> 
+                  </Row>
+                  <Row>
+                   <Col sm>
+                        <Card className='card-card'>
+                            <Card.Img className='card-img' variant="top" src="/assets/expkittensmall.png" />
+                            <Card.Body className='card-body'>
+                                <Card.Title className='card-title'>
+                                    Exploding Kittens
+                                </Card.Title>
+                                <hr/>
+                                <Card.Text className='card-text'>
+                                    Two player card game built to solidify experience using javascript, HTML and CSS.  
+                                </Card.Text>
+                                <hr/>
+                                <Card.Link className='link' href='https://mdcoxe.github.io/explodingkittens/'>
+                                    Deployed
+                                </Card.Link>
+                                <Card.Link className='link' href='https://github.com/mdcoxe/explodingkittens'>
+                                    GitHub Repo
+                                </Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm>    
+                        <Card className='card-card'>
+                            <Card.Img className='card-img' variant="top" src="/assets/honeybadgersmall.png" />
+                            <Card.Body className='card-body'>
+                                <Card.Title className='card-title'>
+                                    Honey Badger Fat-Tire Bike Shop
+                                </Card.Title>
+                                <hr/>
+                                <Card.Text className='card-text'>
+                                    Basic e-commerce store, using NodeJS, Express, MongoDB, and RESTful routing.
+                                </Card.Text>
+                                <hr/>
+                                <Card.Link className='link' href='https://github.com/mdcoxe/honey-badger-fat-tire-bikes'>
+                                    Deployed
+                                </Card.Link>
+                                <Card.Link className='link' href='https://honey-badger-bikes.herokuapp.com/fattire/'>
+                                    GitHub Repo
+                                </Card.Link>
+                            </Card.Body>
+                        </Card> 
+                    </Col>
+             
+                    </Row> 
+                    {/* <Card style={{ width: '50%', marginLeft: '16px' }}>
+                        <Card.Img variant="top" src="/assets/honeybadgersmall.png" />
                         <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>    
-                    <Card className='p-card' style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
+                            <Card.Title>Honey Badger Fat-Tire Bike Shop</Card.Title>
                             <Card.Text>
                             Some quick example text to build on the card title and make up the bulk of
                             the card's content.
@@ -32,9 +130,9 @@ const Projects = () => {
                         </Card.Body>
                     </Card>  
                     <Card className='p-card' style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Img variant="top" src="/assets/trittilosmall.png" />
                         <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
+                            <Card.Title>Trittilo/Tweetr</Card.Title>
                             <Card.Text>
                             Some quick example text to build on the card title and make up the bulk of
                             the card's content.
@@ -42,19 +140,24 @@ const Projects = () => {
                         </Card.Body>
                     </Card>  
                     <Card className='p-card' style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Img variant="top" src="/assets/astrowxsmall.png" />
                         <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
+                            <Card.Title>Astro Wx</Card.Title>
                             <Card.Text>
                             Some quick example text to build on the card title and make up the bulk of
                             the card's content.
                             </Card.Text>
                         </Card.Body>
-                    </Card>  
-                    </CardGroup>
-            </div>              
+                    </Card>   */}
+                    
+            </Container>              
         </div>
     )
 }
 
 export default Projects;
+
+// 
+// https://astro-wx.herokuapp.com/ 
+// https://tweetr-frontend.herokuapp.com/Feed
+// https://tweetr-backend.herokuapp.com/replies
